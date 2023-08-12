5a5e3f14e0b4f8ab4f67a3c922ba77b7f81c9b0e
Benoît Ryder <benoit@ryder.fr>
2023-08-12 21:55:58 +0200

    cdragon-utils: Remove StringError, not used anymore

--- cdragon-utils/src/lib.rs
+++ cdragon-utils/src/lib.rs
@@ -3,20 +3,3 @@ pub mod locale;
 pub mod parsing;
 mod guarded_file;
 pub use guarded_file::GuardedFile;
-
-/// Generic string error
-#[derive(Debug)]
-pub struct StringError(pub String);
-
-impl std::fmt::Display for StringError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str(&self.0)
-    }
-}
-
-impl std::error::Error for StringError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        None
-    }
-}
-
